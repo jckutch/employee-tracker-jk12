59ecb42293606e86604432dc4a66cd09b71a04db
John Kutch <116672999+jckutch@users.noreply.github.com>
2023-02-07 12:10:04 -0500

    working initial prompt, start view list

--- server.js
+++ server.js
@@ -20,6 +20,53 @@ const startPrompt = () => {
 
 const inputResponse = () => {
 
+// view all functions for departments, roles, employee --------
+
+    const viewAllDepartments = () => {
+        const sql = `SELECT id, name FROM departments`;
+        
+        db.query(sql, (err, result) => {
+            if (err) {
+                console.log(err);
+            };
+
+            console.table(result); 
+            selectInput();
+        });
+    }
+
+
+
+
+
+
+// Input selector prompt
+    const selectInput = () => {
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'decision',
+                message: 'What would you like to do?',
+                choices: ['View all departments',
+                            'View all roles',
+                            'View all employees',
+                            'Add a department',
+                            'Add a role',
+                            'Add an employee',
+                            "Update an employee's role",
+                            'Quit'],
+            }
+        ])
+        .then((answers) => {        
+            if (answers.decision === 'View all departments') {
+                viewAllDepartments();
+            }
+        });
+    }
+
+    selectInput();
+}
 
 
 
